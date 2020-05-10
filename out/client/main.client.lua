95671ae309effb02e7dbbf63203cea38d5f35ccf
person4268 <28717044+person4268@users.noreply.github.com>
2020-05-10 02:50:02 -0400

    things

--- out/client/main.client.lua
+++
@@ -1,6 +0,0 @@
--- Compiled with https://roblox-ts.github.io v0.3.2
--- May 9, 2020, 2:42 AM Eastern Daylight Time
-
-local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
-local makeHello = TS.import(script, game:GetService("ReplicatedStorage"), "TS", "module").makeHello;
-print(makeHello("main.client.ts"));
--- out/shared/module.lua
+++
@@ -1,9 +0,0 @@
--- Compiled with https://roblox-ts.github.io v0.3.2
--- May 9, 2020, 2:42 AM Eastern Daylight Time
-
-local exports = {};
-local function makeHello(name)
-	return "Hello from " .. name .. "!";
-end;
-exports.makeHello = makeHello;
-return exports;
