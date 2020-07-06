7ed530503b46baa787ff599ccb1d54e2e9a2dee7
person4268 <28717044+person4268@users.noreply.github.com>
2020-07-06 18:23:02 -0400

    Texutre rotation

--- src/server/textures/TextureHandler.ts
+++ src/server/textures/TextureHandler.ts
@@ -5,12 +5,33 @@ import { Generic5Side1Top } from "./Generic5Side1Top";
 import { Generic4Side1Top1Bottom } from "./Generic4Side1Top1Bottom";
 
 /**
+ * Randomly rotates a block in increments of 90 degrees to make tiling look a little better
+ * @param block 
+ */
+function randomRotate(block: BlockState, rotateOnX: boolean, rotateOnY: boolean, rotateOnZ: boolean) {
+    let possibleRotations = [0, 90, 180, 270];
+    if(rotateOnX) {
+        let rotation = possibleRotations[math.floor(math.random() * 4)];
+        block.block.Orientation = block.block.Orientation.add(new Vector3(rotation, 0, 0));
+    }
+    if(rotateOnY) {
+        let rotation = possibleRotations[math.floor(math.random() * 4)];
+        block.block.Orientation = block.block.Orientation.add(new Vector3(0, rotation, 0));
+    }
+    if(rotateOnZ) {
+        let rotation = possibleRotations[math.floor(math.random() * 4)];
+        block.block.Orientation = block.block.Orientation.add(new Vector3(0, 0, rotation));
+    }
+}
+
+/**
  * Finds the correct TextureHandler for block.id and applies it to the block
  * @param block 
  */
 function applyTexture(block: BlockState) {
     switch (block.id) {
         case "grass":
+            randomRotate(block, false, true, false);
             Generic4Side1Top1Bottom(block, globals.textures.grass_side, globals.textures.grass_top, globals.textures.dirt);
         break;
         case "dirt":
