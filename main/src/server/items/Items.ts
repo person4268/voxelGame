35f73f45bfb720c17502d7715cf2c8a6b34e2edc
person4268 <28717044+person4268@users.noreply.github.com>
2020-12-29 01:56:24 -0500

    started working, this is gonna take a while

--- main/src/server/items/Items.ts
+++ main/src/server/items/Items.ts
@@ -1,4 +1,14 @@
+import { GlobalServerEvent } from "@rbxts/net";
+import globals, { blockIds } from "shared/globals";
+
 //Base class for all items
 export class Item {
+    public maxStack = 64;
+    public name = "Generic Item";
+    public id:blockIds = "generic";
 
+    // Place the item as block as default behaviour. 
+    onUse() {
+
+    }
 }
--- main/src/server/items/ObjectInventory.ts
+++ main/src/server/items/ObjectInventory.ts
@@ -1,4 +1,11 @@
+import { Item } from "./Items";
+
 //Base class for all inventories, a glorified list to some extent
 export class ObjectInventory {
+    items: {count: number, item: Item}[] = [];
+    slotCount = 27;
 
+    addToSlot(position: number, item: Item) {
+
+    }
 }
